26f157bdac8e0fdc60e5dfc1ed65a94566e099ea
Bill Wekesa <wekesabill@gmail.com>
2018-10-11 14:46:39 +0300

    News Container

---
+++ src/components/widgets/NewsList/news-list.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { URL } from '../../../config';
+
+class NewsList extends Component {
+    state= {
+        items:[],
+        start:this.props.start,
+        end:this.props.start + this.props.amount,
+        amount:this.props.amount
+    }
+
+    componentWillMount(){
+        axios.get(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
+        .then ( response =>{
+            this.setState({
+                items:[...this.state.items,...response.data]
+            })
+        })
+    }
+
+    render(){
+        console.log(this.state.items)
+        return (
+            
+            <div>
+
+                Hello
+            </div>
+        )
+    }
+}
+export default NewsList;
