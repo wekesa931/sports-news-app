ce7d9da1df85a81c3fb1450d79cd9fa4ab402186
Bill Wekesa <wekesabill@gmail.com>
2018-10-15 13:40:55 +0300

    code addition

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewsSlider from '../widgets/NewsSlider/slider';
 import NewsList from '../widgets/NewsList/news-list';
+import VideosList from '../widgets/VideosList/videosList';
 
 
 const Home = ()=>{
@@ -18,6 +19,13 @@ const Home = ()=>{
                 type="card"
                 loadmore={true}
                 start={3}
+                amount={3}
+            />
+            <VideosList
+                type="card"
+                title={true}
+                loadmore={false}
+                start={0}
                 amount={3}
             />
         </div>
